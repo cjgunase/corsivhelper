925dbb5b3f31712b6b912ea88dbc6e5a12666453
Chathrua Gunasekara <cjgunase@mtu.edu>
2020-02-13 09:49:55 -0600

    document dataset

---
+++ R/corsiv-data.R
@@ -0,0 +1,14 @@
+#' Prices of 50,000 round cut diamonds
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#'  diamonds. The variables are as follows:
+#'
+
+#'
+#' @docType data
+#' @keywords datasets
+#' @name CoRSIV_Probes
+#' @usage data(CoRSIV_Probes)
+#' @format A data frame with 53940 rows and 10 variables
+NULL
+
